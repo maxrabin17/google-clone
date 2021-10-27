ef349008b190c999a4d3e9d7715730294018808c
maxrabin <max.rabin17@gmail.com>
2021-10-27 13:48:03 -0400

    set up basic router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Home from './pages/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hey There</h1>
-      <Home />
-
-      {/* Home COMP -- PAGE WITH SEARCH BAR */}
-      {/* SearchPage COMP -- RESULTS PAGE */}
+      <Router>
+        <Switch>
+          <Route path="/">
+            <h1>Home Page</h1>
+            <Home />
+          </Route>
+          <Route path="/search">
+            <h1>Search Page</h1>
+            <SearchPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
