5943c622c4462644cc065cbd9fa7baab18b574e3
maxrabin <max.rabin17@gmail.com>
2021-10-27 13:43:35 -0400

    general tidying and basic homepage setup

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>This is Homepage</h1>
+            <div className="homepage_header">
+                <div className="header-right">
+                    
+                </div>
+                <div className="header-left">
+
+                </div>
+            </div>
+
+            <div className="homepage_body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
